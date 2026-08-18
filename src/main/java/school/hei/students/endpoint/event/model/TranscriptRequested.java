f774d5f297ce34a9d30830083faed697b300f7a5
FitiavanaLova <hei.fitiavana@gmail.com>
2026-08-18 11:55:04 +0300

    feat: add transcript requested event

---
+++ src/main/java/school/hei/students/endpoint/event/model/TranscriptRequested.java
@@ -0,0 +1,43 @@
+package school.hei.students.endpoint.event.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Duration;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class TranscriptRequested extends PojaEvent {
+  @JsonProperty("transcriptDeliveryId")
+  private UUID transcriptDeliveryId;
+
+  @JsonProperty("studentId")
+  private UUID studentId;
+
+  @JsonProperty("academicYear")
+  private String academicYear;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+
+  @Override
+  public String getEventSource() {
+    return "school.hei.students.transcript";
+  }
+}
